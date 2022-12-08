a16377055ff560937f6fb90396ec9ff30d394f79
Miaoywww <375629202@qq.com>
2022-12-08 22:21:21 +0800

    每日猫逻辑更新

--- random_cat/__init__.py
+++ random_cat/__init__.py
@@ -9,9 +9,15 @@ miao = on_command("cat", aliases={"jrcat", "今日猫猫", "猫猫"}, block=True
 
 @miao.handle()
 async def hf(bot: Bot, ev: Event):
-    try:
-        async with httpx.AsyncClient() as client:
-            img = await client.get(url="http://edgecats.net/")
-            await bot.send(event=ev, message=MessageSegment.image(img.content))
-    except:
-        return await bot.send(event=ev, message="悲 猫猫获取失败力!")
+    times = 0
+    while True:
+        try:
+            async with httpx.AsyncClient() as client:
+                img = await client.get(url="http://edgecats.net/")
+                await bot.send(event=ev, message=MessageSegment.image(img.content))
+        except:
+            if times < 3:
+                times += 1
+                continue
+            else:
+                return await bot.send(event=ev, message="悲 猫猫获取失败力!")
